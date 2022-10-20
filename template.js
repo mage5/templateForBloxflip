05662e8f206bd34b1fe0c46d989d6ef7813b9343
mage5 <116305510+mage5@users.noreply.github.com>
2022-10-20 19:29:53 +0100

    Update and rename commands/command.js to template.js

---
+++ template.js
@@ -0,0 +1,14 @@
+module.exports = {
+    info: {
+        name: "template",
+        description: "Template",
+        usage: "",
+        aliases: []
+    },
+
+    run: async function(client, message, args){
+
+
+ 
+    }
+}

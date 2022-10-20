3ed47b7cf10a2a6daf9ccb00fc54f5868da343cf
mage5 <116305510+mage5@users.noreply.github.com>
2022-10-20 20:51:51 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,3 @@
-const { Collection, Client, ActivityType } = require("discord.js");
-
 module.exports = async (client) => {
     console.log(`[BOT] Logged in as ${client.user.username}`);
-    client.user.setActivity(`${client.guilds.cache.size} Guilds with ${client.users.cache.size} Users`, { type: ActivityType.Watching })
-    //The activity ActivityType can be Competing, Listening, Playing, Streaming, or Watching
   };
